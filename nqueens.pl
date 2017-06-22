9d749a5f2e973cc8e7c53472b46e771ef8fc3f49
Duanyang Jing <gmjingdy@gmail.com>
2017-06-21 21:01:37 -0500

    nqueens: remove unused var in some relations
    Also change comments to logic style

--- nqueens.pl
+++ nqueens.pl
@@ -1,18 +1,19 @@
-% attack range for queen at pos(X, Y)
-attack(N, pos(X, Y), pos(X1, Y)).
-attack(N, pos(X, Y), pos(X, Y1)).
-attack(N, pos(X, Y), pos(X1, Y1)) :- abs(X1 - X) =:= abs(Y1 - Y).
+% attack holds if two queens at the two positions can attack each other
+% This can happen for three cases: horizontal, vertical or diagonal
+attack(pos(_, Y), pos(_, Y)).
+attack(pos(X, _), pos(X, _)).
+attack(pos(X, Y), pos(X1, Y1)) :- abs(X1 - X) =:= abs(Y1 - Y).
 
 
-% P is a valid position given a list of moves
-validPos(N, [], P).
-validPos(N, [H|T], P) :- not(attack(N, H, P)), validPos(N, T, P).
+% validPos holds if P is a valid position given a list of moves
+validPos([], _).
+validPos([H|T], P) :- not(attack(H, P)), validPos(T, P).
 
 
-% pos(X, Y) is a valid next step given a list of moves
+% next holds if pos(X, Y) is a valid next step given a list of moves
 next(N, X, Moves, pos(X, Y)) :-
 	between(1, N, Y),
-	validPos(N, Moves, pos(X, Y)).
+	validPos(Moves, pos(X, Y)).
 
 nqueens_aux(N, N, Moves, Moves).
 nqueens_aux(N, X, Moves, Ans) :- 
